825ab2c4907123cfc1145c4df49ca9c385e3c436
Jacob Gadikian <faddat@gmail.com>
2015-10-27 14:45:18 +0700

    Update rancheros.sh

--- rancheros.sh
+++ rancheros.sh
@@ -1,1 +1,6 @@
+#! /bin/bash
+rm vmlinuz
+rm initrd
+wget -O vmlinuz https://releases.rancher.com/os/latest/vmlinuz
+wget -O initrd https://releases.rancher.com/os/latest/initrd
 pixiecore -kernel=vmlinuz -initrd=initrd

7783096c3e2e71edb3cd5d688cec512c89f039de
Jacob Gadikian <faddat@gmail.com>
2015-10-27 14:32:04 +0700

    https changed to http

--- rancheros.sh
+++ rancheros.sh
@@ -1,1 +1,1 @@
-pixiecore -kernel=https://releases.rancher.com/os/latest/vmlinuz -initrd=http://releases.rancher.com/os/latest/initrd
+pixiecore -kernel=http://releases.rancher.com/os/latest/vmlinuz -initrd=http://releases.rancher.com/os/latest/initrd
